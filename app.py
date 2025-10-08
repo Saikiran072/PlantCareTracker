1bf4c14b4a0667096005f28ebf17a0be594cbda3
P Sai Kiran Goud <120726655+Saikiran072@users.noreply.github.com>
2025-10-08 23:00:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -350,9 +350,10 @@ def add_journal_entry(id):
     return render_template("add_journal_entry.html", form=form, plant=plant)
 
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
 
